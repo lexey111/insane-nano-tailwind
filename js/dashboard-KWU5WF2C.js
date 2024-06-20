de6950794fa3da233900fe1bfcb33a967d2170d9
Oleksii Koshkin <lexey111@gmail.com>
2024-06-20 17:21:26 +0200

    Initial commit

---
+++ js/dashboard-KWU5WF2C.js
@@ -0,0 +1,2 @@
+"use strict";(()=>{console.log("Dashboard init - tailwind");})();
+//# sourceMappingURL=dashboard-KWU5WF2C.js.map
---
+++ js/main-4UOIADPZ.js
@@ -0,0 +1,2 @@
+"use strict";(()=>{function o(e){console.log("Hello World:",e.toString())}console.log("Main init - tailwind");o(42);})();
+//# sourceMappingURL=main-4UOIADPZ.js.map

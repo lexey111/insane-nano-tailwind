c91d924d2be689d01c3c773139f42caf4b17d967
Oleksii Koshkin <lexey111@gmail.com>
2024-06-20 17:28:19 +0200

    fix deps

---
+++ dist/js/dashboard-G4NQ7LWS.js
@@ -0,0 +1,6 @@
+new EventSource("/esbuild").addEventListener("change", () => setTimeout(() => location.reload(), 1000));
+"use strict";
+(() => {
+  // src/js/dashboard.js
+  console.log("Dashboard init - tailwind");
+})();
